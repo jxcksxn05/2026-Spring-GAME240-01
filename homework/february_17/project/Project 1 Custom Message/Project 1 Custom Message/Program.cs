9c68700771cfbe2be62d44525b6b67040eff4b81
jxcksxn05 <jackdavis@chapman.edu>
2026-02-17 16:01:18 -0800

    This is my C# program with a custom message. It is for the homework assignment due February 17th.

---
+++ homework/february_17/project/Project 1 Custom Message/Project 1 Custom Message/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("> Hello, what is your name?");
+
+var name = Console.ReadLine();
+
+Console.WriteLine("> Hello there, " +name+ ".");
+
+Console.WriteLine("> Is it your birthday?");
+
+var isBirthday = Console.ReadLine();
+
+if (isBirthday == "yes")
+{
+    Console.WriteLine("> Well, happy birthday!");
+    Console.WriteLine("> How old are you, "+name+"?");
+
+    var age = Console.ReadLine();
+
+    Console.WriteLine("> That's great, "+name+"! You are "+age+" years old!");
+}
+
+else if (isBirthday == "no")
+{
+    Console.WriteLine("> Well I hope you are looking forward to it!");
+}
+else
+{
+    Console.WriteLine("> Huh?");
+}
